2959f7eb457419af5c35b62d57c5d129891dac01
James Min <jmin499@gmail.com>
2019-10-11 18:04:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,44 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  males = data[:gender][:male]
+  females = data[:gender][:female]
+  names = males.concat(females)
+
+  new_hash = {}
+  for name in names
+    colors = data[:color].keys
+    genders = data[:gender].keys
+    lives = data[:lives].keys
+    
+    arr_colors = []
+    arr_gender = []
+    arr_lives =[]
+
+    colors.each { |color|
+      if data[:color][color].include?(name)
+        arr_colors.push(color.to_s)
+      end
+    }
+    
+    genders.each { |gender|
+      if data[:gender][gender].include?(name)
+        arr_gender = [gender.to_s]
+      end
+    }
+
+    lives.each { |live|
+      if data[:lives][live].include?(name)
+        arr_lives.push(live.to_s)
+      end
+    }
+
+    new_hash[name] = {
+      :color => arr_colors,
+      :gender => arr_gender,
+      :lives => arr_lives
+    }
+    
+  end
+
+  new_hash
 end
